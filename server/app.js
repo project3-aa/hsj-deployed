bda13c4d7d8e723fac1648bb13ce42af66012bb4
Andre Cabrera <cabrerandre@gmail.com>
2019-08-08 13:04:05 -0400

    hfgdfsd

--- server/app.js
+++ server/app.js
@@ -31,7 +31,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
     
 
