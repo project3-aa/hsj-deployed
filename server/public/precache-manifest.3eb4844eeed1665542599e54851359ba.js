5e2f9f1ca986c3328a1e18940b4f14d385840e86
Andre Cabrera <cabrerandre@gmail.com>
2019-08-12 17:55:40 -0400

    ui improvements

---
+++ server/public/precache-manifest.3eb4844eeed1665542599e54851359ba.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4275a28667e81dc911790b9766e26a41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2260ef411a99e46fc46e",
+    "url": "/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "2f74dff7c063f881d3a1",
+    "url": "/static/css/3.f0b3530c.chunk.css"
+  },
+  {
+    "revision": "91447cfe61ebf55e78f6",
+    "url": "/static/css/4.cdf59009.chunk.css"
+  },
+  {
+    "revision": "154110c04dab88697a4b",
+    "url": "/static/css/5.f79ec62c.chunk.css"
+  },
+  {
+    "revision": "419367690d1caa3eb09a",
+    "url": "/static/css/6.fa2cbb12.chunk.css"
+  },
+  {
+    "revision": "339dd663bb574a895246",
+    "url": "/static/css/7.621d226c.chunk.css"
+  },
+  {
+    "revision": "309e82967190da4d42b7",
+    "url": "/static/css/main.cf77c240.chunk.css"
+  },
+  {
+    "revision": "2260ef411a99e46fc46e",
+    "url": "/static/js/2.1c2316fa.chunk.js"
+  },
+  {
+    "revision": "2f74dff7c063f881d3a1",
+    "url": "/static/js/3.23b2f012.chunk.js"
+  },
+  {
+    "revision": "91447cfe61ebf55e78f6",
+    "url": "/static/js/4.496d2d21.chunk.js"
+  },
+  {
+    "revision": "154110c04dab88697a4b",
+    "url": "/static/js/5.20e4ba17.chunk.js"
+  },
+  {
+    "revision": "419367690d1caa3eb09a",
+    "url": "/static/js/6.9e552aba.chunk.js"
+  },
+  {
+    "revision": "339dd663bb574a895246",
+    "url": "/static/js/7.6d49b44c.chunk.js"
+  },
+  {
+    "revision": "309e82967190da4d42b7",
+    "url": "/static/js/main.6edd327f.chunk.js"
+  },
+  {
+    "revision": "886d98433b094ae0a50b",
+    "url": "/static/js/runtime~main.5a55de2e.js"
+  }
+]);
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e115f9bb2656faf38b26d45c867afa4e.js"
+  "/precache-manifest.3eb4844eeed1665542599e54851359ba.js"
 );
 
 self.addEventListener('message', (event) => {
