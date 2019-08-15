0284f524f1b35f4bac5dfc45dfee5eec3d5ba5a2
Andre Cabrera <cabrerandre@gmail.com>
2019-08-15 11:33:06 -0400

    run build for heroku

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.26fa94e7517f474a1f0621ba9c1c27d5.js"
+  "/precache-manifest.f9e77e8441149b297f94ca4a7788a4c7.js"
 );
 
 self.addEventListener('message', (event) => {
